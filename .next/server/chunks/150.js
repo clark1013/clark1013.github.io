d02fe64567f583e5d3dea323844c1c971551da70
Wen Jiazhi <jiazhi.wen@pingcap.com>
2023-07-23 14:47:15 +0800

    init contentlayer templates

---
+++ .next/server/chunks/150.js
@@ -0,0 +1,57 @@
+exports.id = 150;
+exports.ids = [150];
+exports.modules = {
+
+/***/ 9639:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 9222, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 8301, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3751, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 4765, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 5192, 23))
+
+/***/ }),
+
+/***/ 9441:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 9274, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3349, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3280, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 2144, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 408, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6690, 23))
+
+/***/ }),
+
+/***/ 9396:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "i": () => (/* binding */ Mdx)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2208);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_contentlayer_hooks__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(7382);
+
+
+
+const components = {
+    Image: (next_image__WEBPACK_IMPORTED_MODULE_1___default())
+};
+function Mdx({ code  }) {
+    const Component = (0,next_contentlayer_hooks__WEBPACK_IMPORTED_MODULE_2__/* .useMDXComponent */ .z)(code);
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
+        components: components
+    });
+}
+
+
+/***/ })
+
+};
+;
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# clark1013.github.io
+# Next.js + Contentlayer
+
+A template with Next.js 13 app dir, Contentlayer, Tailwind CSS and dark mode.
+
+https://next-contentlayer.vercel.app
+
+
